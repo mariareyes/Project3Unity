afec39b9d167cf55cbdc6f3b428a0ad68cc3656a
Maria <mariafreaner@gmail.com>
2016-12-13 18:13:07 -0500

    project 3
    Project 3 Solar System

---
+++ Nov11Class/Assets/ChangeMaterial.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeMaterial : MonoBehaviour {
+    public Material materialToChangeTo;
+    public void OnCollisionEnter(Collision collider) {
+        Debug.Log("k");
+
+
+    }
+}
---
+++ Nov11Class/Assets/appydirection.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class appydirection : MonoBehaviour {
+    public Vector3 direction;
+
+    //Whether or not we want to use discrete-styled movement
+    public bool isDiscrete = true;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (true == Input.GetKeyDown(KeyCode.Space)) {
+            if (isDiscrete)
+            {
+                transform.localPosition += direction;
+            }
+            else {
+                //Stores any info relevant to a collision
+                RaycastHit hit;
+               bool didHit= Physics.Raycast(transform.position,//where we start the ray from
+                    direction,//direction of the ray
+                    out hit,//variable to store results of any collision
+                    direction.magnitude,//length of the ray
+                    LayerMask.NameToLayer("Default"));//layer we want to collide
+                if (didHit) {
+                    Debug.Log("DID HIT! BITCH");
+                }
+            }
+        }
+	}
+}
